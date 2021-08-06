70c2d71d10a3ad37fa4fb5be80d39de515ad1234
sacden <sacden2009@gmail.com>
2021-08-06 13:14:20 +0200

    add template and css

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-import bootstrap from 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
-import './src/services/services'
-import './src/components/createBigPhoto/createBigPhoto'
-import './src/components/createListItems/createListItems'
-import './src/components/loadMoreButton/loadMoreButton'
-import './src/components/serchForm/serchForm'
+import bootstrap from "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import "./src/services/services";
+import "./src/components/createBigPhoto/createBigPhoto";
+import "./src/components/createListItems/createListItems";
+import "./src/components/loadMoreButton/loadMoreButton";
+import "./src/components/serchForm/serchForm";
+import { createLogo } from "./src/components/createLogo/createLogo";
 
+console.log("hhhh");
 
-console.log('hhhh')
+const logo = new createLogo();
+logo.render();

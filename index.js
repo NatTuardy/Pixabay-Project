b701e10188e1611debb4c9146b48e08d71ce3612
sacden <sacden2009@gmail.com>
2021-08-09 17:16:05 +0200

    add card lists

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import bootstrap from "bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import "./src/services/services";
@@ -7,8 +8,12 @@ import "./src/components/createListItems/createListItems";
 import "./src/components/loadMoreButton/loadMoreButton";
 import "./src/components/serchForm/serchForm";
 import { createLogo } from "./src/components/createLogo/createLogo";
+import { createListItems } from "./src/components/createListItems/createListItems";
 
 console.log("hhhh");
 
 const logo = new createLogo();
 logo.render();
+
+const listItems = new createListItems();
+listItems.render();
--- src/components/createListItems/createListItems.js
+++ src/components/createListItems/createListItems.js
@@ -1,1 +1,34 @@
-console.log('list')
+console.log("list");
+import CreateRequest from "../../services/services";
+
+class createListItems {
+  render() {
+    let imageContainer = document.createElement("div");
+    imageContainer.className = "container-sm row row-cols-1 row-cols-md-3 g-4";
+    imageContainer.id = "images";
+    document.body.append(imageContainer);
+
+    const newRequest = new CreateRequest();
+    let apiData = newRequest.render();
+    apiData.then((data) => {
+      data.forEach((element) => {
+        imageContainer.insertAdjacentHTML(
+          "beforeend",
+          `<div class="card">
+    <img src="${element.largeImageURL}" class="card-img-top" alt="...">
+    <div class="card-footer">
+    <small class="text-muted">
+    <i class="bi bi-heart">${element.likes}</i>
+    <i class="bi bi-card-text">${element.comments}</i>
+    <i class="bi bi-eye">${element.views}</i>
+    <i class="bi bi-cloud-download">${element.downloads}</i>
+    </small>
+    </div>
+    </div>`
+        );
+      });
+    });
+  }
+}
+
+export { createListItems };

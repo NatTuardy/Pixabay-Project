d3d2c65b2c6e424649c22be1182d0ad83f8fef1e
sergeevev <sergeevevit@gmail.com>
2021-08-10 13:46:06 +0500

    add render search form and get api-data

--- src/components/serchForm/serchForm.js
+++ src/components/serchForm/serchForm.js
@@ -1,15 +1,19 @@
+import { DocumentPosition } from 'domutils'
+import CreateRequest from '../../services/services'
+
 console.log('searchForm')
 
 export default class SearchForm {
      #container
 
      constructor() {
-          
           this.#container = document.createElement('div')
-
+          this.createRequest = new CreateRequest()
      }
 
      render() {
+
+          const mainContainer = document.querySelector('#main-container')
 
           const searchForm = document.createElement('form')
           searchForm.className = 'input-group input-group-lg w-50 p-3'
@@ -22,16 +26,58 @@ export default class SearchForm {
           searchFormInput.autocomplete = 'off'
           searchFormInput.placeholder = 'Search images...'
 
-          const spanFormButton = document.createElement('span')
-          spanFormButton.className = 'input-group-text'
-          spanFormButton.id = 'basic-addon2'
-          spanFormButton.textContent = 'SEARCH'
+          const submitFormButton = document.createElement('button')
+          submitFormButton.type = 'submit'
+          submitFormButton.className = 'input-group-text'
+          submitFormButton.id = 'basic-addon2'
+          submitFormButton.textContent = 'SEARCH'
+
+          searchForm.addEventListener('submit', (event) => {
+               event.preventDefault();
+               this.createRequest.resetPage()
+
+               //чистим галерею
+               // const galleryDiv = document.querySelector('.')
+               // galleryDiv.remove()
+
+               this.createRequest.setUrl(searchFormInput.value)
+
+               // const getAnswer = async () => {
+               
+               //      try {
+                         const response = this.createRequest.render()
+                         console.log(response)
+                         // if (!response.ok) {
+                         //      throw new Error('Произошла ошибка в получении данных об альбомах...')
+                         // }
+                         // const result = await response.json()
+                         
+                         // result.forEach((album) => {
+                         //      const albumHTML = createAlbumElement(album.title)
+                         //      dataContainer.append(albumHTML)
+                         // })
+                    // }
+                    // catch(error) {
+                    //      console.log ('error', error)
+                    // }
+                    // finally {
+                    //      console.log ('finally')
+                    //      toggleLoader()
+                    // }
+               // }
+
+               // getAnswer()
+          })
 
           this.#container.append(searchForm)
-          searchForm.append(searchFormInput, spanFormButton)       
+          searchForm.append(searchFormInput, submitFormButton)     
 
-          return this.#container
+          mainContainer.append(this.#container)
+          // return this.#container
+          
+     }
 
-          }
-     
 }
+
+const newForm = new SearchForm()
+newForm.render()
